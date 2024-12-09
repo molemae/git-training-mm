9f5feb56c9124a0c35879fb2d28b3fda94ee334b
moritz.maehrlein <moritz.maehrlein@now-gmbh.de>
2024-12-09 14:09:36 +0100

    main.py adjustment

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-# This is a sample Python script.
+# This is a sample Python script. featurefix
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
+def print_hi(name): # featurefix
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hey man, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
+    print_hi('PyCharm') # featurefix
+# hier schreiben wir im feature noch weiter
 # See PyCharm help at https://www.jetbrains.com/help/pycharmsetopt NO_HIST_EXPANSION
