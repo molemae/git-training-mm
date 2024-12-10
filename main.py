6815f37a16dd6abc65e46ed84e3d0589c1bcbf84
moritz.maehrlein <moritz.maehrlein@now-gmbh.de>
2024-12-10 13:10:09 +0100

    brummli

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
-
+#brummli brummli
 def print_hi(x_name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hey man, {x_name}')  # Press ⌘F8 to toggle the breakpoint.
